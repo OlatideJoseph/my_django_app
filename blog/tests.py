7d4ab20eb0081c2251dfef6439b5446cadada8cf
Olatide Joseph <=olatidejoseph17@gmail.com>
2023-02-14 00:55:04 -0800

    this is a new update

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class TestIndex(SimpleTestCase):
+	def test_url_code_index(self):
+		response=self.client.get("/")
+		self.assertEqual(response.status_code,200)
+class TestPortfolio(SimpleTestCase):
+	def test_url_code_portfolio(self):
+		response=self.client.get("/olatide-joseph-adeniyi")
+		self.assertEqual(response.status_code,200)
+	def test_endpoint_url(self):
+		self.assertEqual(reverse('olatide'),"/olatide-joseph-adeniyi")
+
